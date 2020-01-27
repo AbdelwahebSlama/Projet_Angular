8eb0666a6f4fbbc82e1c13b1c3824e39f3932c99
Slama Abdelwaheb <Abdelwaheb.Slama@yahoo.com>
2020-01-27 21:32:07 +0100

    First component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExempleComponent } from './Components/exemple/exemple.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExempleComponent
   ],
   imports: [
     BrowserModule,
